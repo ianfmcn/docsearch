21b8d60798598ffa0a13e31d3f4b14af15fb2338
ianfmcn <114420968+ianfmcn@users.noreply.github.com>
2022-10-20 10:24:28 -0700

    lab4

---
+++ DocSearchTest.java
@@ -0,0 +1,8 @@
+
+class DocSearchTest {
+    public void HandlerTest() {
+        URI testUrl = new URI("https://ucsd-cse15l-f22.github.io/week/week4/");
+        System.out.println(handleRequest());
+    }
+
+}
